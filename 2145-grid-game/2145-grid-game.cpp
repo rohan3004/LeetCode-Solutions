257ddf1bd80e08e34cbf8ced147f892870a6207f
Rohan Chakravarty <112606377+rohan3004@users.noreply.github.com>
2025-01-21 22:23:33 +0530

    Time: 5 ms (35.75%), Space: 66.7 MB (76.92%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
+
+        //First Row Sum, Second Row Sum, and Answer
         long long fRowSum=0, sRowSum=0, ans = LONG_LONG_MAX;
         for(int i=0;i<grid[0].size();i++)fRowSum+=grid[0][i];
+        //Count the First Row Sum
 
         for(int i=0;i<grid[0].size();i++){
-            fRowSum-=grid[0][i];
+            fRowSum-=grid[0][i];//Points of the robot 2 depends on the column that robot 1 takes
 
-            long long robot2Best = max(fRowSum,sRowSum);
+            long long robot2Best = max(fRowSum,sRowSum); //Robot 2 playes optimally to choose its best
 
-            sRowSum+=grid[1][i];
+            sRowSum+=grid[1][i]; //Second Row Sum that robot 2 can take for the next iteration, current iteration robot 1 takes
 
-            ans = min(ans,robot2Best);
+            ans = min(ans,robot2Best); //Robot 1 takes that decision that will be lead to minimum for the robot 2 
         }
         
         return ans;
